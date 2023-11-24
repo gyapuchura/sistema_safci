20c2e3e4bfc8c8a5bf59d11385bb5d3242401a8d
Luis Gonzalo Yapuchura Chuquimia <luis_yapuchura@contraloria.gob.bo>
2023-11-24 14:47:11 -0400

    modulo reportes personal

--- recursos_humanos/reportes_personal.php
+++ recursos_humanos/reportes_personal.php
@@ -58,7 +58,7 @@ $perfil_ss     =  $_SESSION['perfil_ss'];
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">RECURSOS HUMANOS SAFCI - MUNICIPIO</h1>
+                    <h1 class="h3 mb-2 text-gray-800">REPORTES RECURSOS HUMANOS SAFCI</h1>
                     <p class="mb-4">En esta seccion se puede encontrar el registro de recusos humanos del PROGBRAMA NACIONAL SAFCI - MI SALUD.</p>
 
             <!-- REPORTE POR DEPARTAMENTO -->
