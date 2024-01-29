e9e6555777a2d7541c628194ce3a1df1bd50f95b
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-01-29 11:56:41 -0400

    actualizcion grupos etareos: municipios-establecimientos

---
+++ implementacion_safci/piramide_sospechas.php
@@ -0,0 +1,288 @@
+<?php include("../cabf.php");?>
+<?php include("../inc.config.php");?>
+<?php
+date_default_timezone_set('America/La_Paz');
+$fecha_ram	    = date("Ymd");
+$fecha 		    = date("Y-m-d");
+$gestion        = date("Y");
+
+$idsospecha_diag_nal='23';
+$iddepartamento_ep   = '4';
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Highcharts Example</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+    var categories = [
+                    <?php
+                    $numero = 0;
+                    $sql = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                    $result = mysqli_query($link,$sql);
+                    $total = mysqli_num_rows($result);
+                    if ($row = mysqli_fetch_array($result)){
+                    mysqli_field_seek($result,0);
+                    while ($field = mysqli_fetch_field($result)){
+                    } do {
+                    ?>
+                    '<?php  echo $row[1];?>'
+
+                        <?php    
+                        $numero++;
+                        if ($numero == $total) { echo "";} else {echo ",";}
+
+                    } while ($row = mysqli_fetch_array($result));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>
+        ];
+    $(document).ready(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'SOSPECHAS DIAGNÓSTICAS A NIVEL NACIONAL'
+            },
+            subtitle: {
+                text: 'Fuente: Sistema Medi-Safci'
+            },
+            xAxis: [{
+                categories: categories,
+                reversed: false,
+                labels: {
+                    step: 1
+                }
+            }, { // mirror axis on right side
+                opposite: true,
+                reversed: false,
+                categories: categories,
+                linkedTo: 0,
+                labels: {
+                    step: 1
+                }
+            }],
+            yAxis: {
+                title: {
+                    text: null
+                },
+                labels: {
+                    formatter: function () {
+                        return (Math.abs(this.value) / 10) + 'x10';
+                    }
+                },
+                min: -20,
+                max: 20
+            },
+
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' +
+                        'Sospechas: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+                }
+            },
+
+            series: [{
+                name: 'MASCULINO',
+                data: [
+                    <?php
+                    $numero2 = 0;
+                    $sql2 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                    $result2 = mysqli_query($link,$sql2);
+                    $total2 = mysqli_num_rows($result2);
+                    if ($row2 = mysqli_fetch_array($result2)){
+                    mysqli_field_seek($result2,0);
+                    while ($field2 = mysqli_fetch_field($result2)){
+                    } do {
+                    ?>
+
+                    <?php
+                    $sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+                    $sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+                    $sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='2' AND registro_enfermedad.idgrupo_etareo='$row2[0]'";
+                    $sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.iddepartamento='$iddepartamento_ep' ";
+                    $result7 = mysqli_query($link,$sql7);
+                    $row7 = mysqli_fetch_array($result7);
+                    $cifra_masculino = $row7[0];
+                    ?>
+                                <?php echo "-".$cifra_masculino; ?>
+
+                        <?php    
+                        $numero2++;
+                        if ($numero2 == $total2) { echo "";} else {echo ",";}
+
+                    } while ($row2 = mysqli_fetch_array($result2));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>                   
+                ]
+            }, {
+                name: 'FEMENINO',
+                data: [
+                    <?php
+                        $numero3 = 0;
+                        $sql3 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                        $result3 = mysqli_query($link,$sql3);
+                        $total3 = mysqli_num_rows($result3);
+                        if ($row3 = mysqli_fetch_array($result3)){
+                        mysqli_field_seek($result3,0);
+                        while ($field3 = mysqli_fetch_field($result3)){
+                        } do {
+                        ?>
+
+                        <?php
+                        $sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+                        $sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+                        $sql7.= " AND registro_enfermedad.idsospecha_diag='23' AND registro_enfermedad.idgenero='1' AND registro_enfermedad.idgrupo_etareo='$row3[0]'";
+                        $sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.iddepartamento='$iddepartamento_ep' ";
+                        $result7 = mysqli_query($link,$sql7);
+                        $row7 = mysqli_fetch_array($result7);
+                        $cifra_femenino = $row7[0];
+                        ?>
+                                    <?php echo $cifra_femenino; ?>
+
+                            <?php    
+                            $numero3++;
+                            if ($numero3 == $total3) { echo "";} else {echo ",";}
+
+                        } while ($row3 = mysqli_fetch_array($result3));
+                        } else {
+                        /*
+                        Si no se encontraron resultados
+                        */
+                        }
+                        ?>                   
+                ]
+            }]
+        });
+    });
+
+});
+		</script>
+	</head>
+	<body>
+<script src="../js/highcharts.js"></script>
+<script src="../js/modules/exporting.js"></script>
+
+<div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+
+<?php
+$numero = 0;
+$sql = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result = mysqli_query($link,$sql);
+$total = mysqli_num_rows($result);
+ if ($row = mysqli_fetch_array($result)){
+mysqli_field_seek($result,0);
+while ($field = mysqli_fetch_field($result)){
+} do {
+?>
+   '<?php  echo $row[1];?>'
+
+    <?php    
+    $numero++;
+    if ($numero == $total) { echo "";} else {echo ",";}
+
+} while ($row = mysqli_fetch_array($result));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+
+</br>
+</br>
+
+<?php
+$numero2 = 0;
+$sql2 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result2 = mysqli_query($link,$sql2);
+$total2 = mysqli_num_rows($result2);
+ if ($row2 = mysqli_fetch_array($result2)){
+mysqli_field_seek($result2,0);
+while ($field2 = mysqli_fetch_field($result2)){
+} do {
+?>
+
+<?php
+$sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+$sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+$sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='2' AND registro_enfermedad.idgrupo_etareo='$row2[0]'";
+$sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.iddepartamento='4' ";
+$result7 = mysqli_query($link,$sql7);
+$row7 = mysqli_fetch_array($result7);
+$cifra_masculino = $row7[0];
+?>
+             <?php echo "-".$cifra_masculino; ?>
+
+    <?php    
+    $numero2++;
+    if ($numero2 == $total2) { echo "";} else {echo ",";}
+
+} while ($row2 = mysqli_fetch_array($result2));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+</br>
+</br>
+
+<?php
+$numero3 = 0;
+$sql3 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result3 = mysqli_query($link,$sql3);
+$total3 = mysqli_num_rows($result3);
+ if ($row3 = mysqli_fetch_array($result3)){
+mysqli_field_seek($result3,0);
+while ($field3 = mysqli_fetch_field($result3)){
+} do {
+?>
+
+<?php
+$sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+$sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+$sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='1' AND registro_enfermedad.idgrupo_etareo='$row3[0]'";
+$sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.iddepartamento='4' ";
+$result7 = mysqli_query($link,$sql7);
+$row7 = mysqli_fetch_array($result7);
+$cifra_femenino = $row7[0];
+?>
+             <?php echo $cifra_femenino; ?>
+
+    <?php    
+    $numero3++;
+    if ($numero3 == $total3) { echo "";} else {echo ",";}
+
+} while ($row3 = mysqli_fetch_array($result3));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+	</body>
+</html>
---
+++ implementacion_safci/piramide_sospechas_nal.php
@@ -0,0 +1,300 @@
+<?php include("../cabf.php");?>
+<?php include("../inc.config.php");?>
+<?php
+date_default_timezone_set('America/La_Paz');
+$fecha_ram	    = date("Ymd");
+$fecha 		    = date("Y-m-d");
+$gestion        = date("Y");
+
+$idsospecha_diag_nal=$_GET['sospecha_diag_nal'];
+
+$sql_sos = " SELECT idsospecha_diag, sospecha_diag FROM sospecha_diag WHERE idsospecha_diag='$idsospecha_diag_nal' ";
+$result_sos = mysqli_query($link,$sql_sos);
+$row_sos = mysqli_fetch_array($result_sos);
+
+$sqlav = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+$sqlav.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+$sqlav.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' ";
+$sqlav.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' ";
+$resultav = mysqli_query($link,$sqlav);
+$rowav = mysqli_fetch_array($resultav);
+$regulador = $rowav[0]/4;
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>SOSPECHAS-ETAREO-SAFCI</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+    var categories = [
+                    <?php
+                    $numero = 0;
+                    $sql = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                    $result = mysqli_query($link,$sql);
+                    $total = mysqli_num_rows($result);
+                    if ($row = mysqli_fetch_array($result)){
+                    mysqli_field_seek($result,0);
+                    while ($field = mysqli_fetch_field($result)){
+                    } do {
+                    ?>
+                    '<?php  echo $row[1];?>'
+
+                        <?php    
+                        $numero++;
+                        if ($numero == $total) { echo "";} else {echo ",";}
+
+                    } while ($row = mysqli_fetch_array($result));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>
+        ];
+    $(document).ready(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'SOSPECHAS <?php echo $row_sos[1];?> A NIVEL NACIONAL'
+            },
+            subtitle: {
+                text: 'Fuente: Sistema Medi-Safci'
+            },
+            xAxis: [{
+                categories: categories,
+                reversed: false,
+                labels: {
+                    step: 1
+                }
+            }, { // mirror axis on right side
+                opposite: true,
+                reversed: false,
+                categories: categories,
+                linkedTo: 0,
+                labels: {
+                    step: 1
+                }
+            }],
+            yAxis: {
+                title: {
+                    text: null
+                },
+                labels: {
+                    formatter: function () {
+                        return (Math.abs(this.value) / 100) + 'x100';
+                    }
+                },
+                min: -<?php echo $regulador;?>,
+                max: <?php echo $regulador;?>
+            },
+
+            plotOptions: {
+                series: {
+                    stacking: 'normal'
+                }
+            },
+
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + ',  ' + this.point.category + '</b><br/>' +
+                        'Sospechas: ' + Highcharts.numberFormat(Math.abs(this.point.y), 0);
+                }
+            },
+
+            series: [{
+                name: 'MASCULINO',
+                data: [
+                    <?php
+                    $numero2 = 0;
+                    $sql2 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                    $result2 = mysqli_query($link,$sql2);
+                    $total2 = mysqli_num_rows($result2);
+                    if ($row2 = mysqli_fetch_array($result2)){
+                    mysqli_field_seek($result2,0);
+                    while ($field2 = mysqli_fetch_field($result2)){
+                    } do {
+                    ?>
+
+                    <?php
+                    $sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+                    $sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+                    $sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='2' AND registro_enfermedad.idgrupo_etareo='$row2[0]'";
+                    $sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' ";
+                    $result7 = mysqli_query($link,$sql7);
+                    $row7 = mysqli_fetch_array($result7);
+                    $cifra_masculino = $row7[0];
+                    ?>
+                                <?php echo "-".$cifra_masculino; ?>
+
+                        <?php    
+                        $numero2++;
+                        if ($numero2 == $total2) { echo "";} else {echo ",";}
+
+                    } while ($row2 = mysqli_fetch_array($result2));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>                   
+                ]
+            }, {
+                name: 'FEMENINO',
+                data: [
+                    <?php
+                        $numero3 = 0;
+                        $sql3 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+                        $result3 = mysqli_query($link,$sql3);
+                        $total3 = mysqli_num_rows($result3);
+                        if ($row3 = mysqli_fetch_array($result3)){
+                        mysqli_field_seek($result3,0);
+                        while ($field3 = mysqli_fetch_field($result3)){
+                        } do {
+                        ?>
+
+                        <?php
+                        $sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+                        $sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+                        $sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='1' AND registro_enfermedad.idgrupo_etareo='$row3[0]'";
+                        $sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' ";
+                        $result7 = mysqli_query($link,$sql7);
+                        $row7 = mysqli_fetch_array($result7);
+                        $cifra_femenino = $row7[0];
+                        ?>
+                                    <?php echo $cifra_femenino; ?>
+
+                            <?php    
+                            $numero3++;
+                            if ($numero3 == $total3) { echo "";} else {echo ",";}
+
+                        } while ($row3 = mysqli_fetch_array($result3));
+                        } else {
+                        /*
+                        Si no se encontraron resultados
+                        */
+                        }
+                        ?>                   
+                ]
+            }]
+        });
+    });
+
+});
+		</script>
+	</head>
+	<body>
+<script src="../js/highcharts.js"></script>
+<script src="../js/modules/exporting.js"></script>
+
+<div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+
+<?php
+$numero = 0;
+$sql = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result = mysqli_query($link,$sql);
+$total = mysqli_num_rows($result);
+ if ($row = mysqli_fetch_array($result)){
+mysqli_field_seek($result,0);
+while ($field = mysqli_fetch_field($result)){
+} do {
+?>
+   '<?php  echo $row[1];?>'
+
+    <?php    
+    $numero++;
+    if ($numero == $total) { echo "";} else {echo ",";}
+
+} while ($row = mysqli_fetch_array($result));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+
+</br>
+</br>
+
+<?php
+$numero2 = 0;
+$sql2 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result2 = mysqli_query($link,$sql2);
+$total2 = mysqli_num_rows($result2);
+ if ($row2 = mysqli_fetch_array($result2)){
+mysqli_field_seek($result2,0);
+while ($field2 = mysqli_fetch_field($result2)){
+} do {
+?>
+
+<?php
+$sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+$sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+$sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='2' AND registro_enfermedad.idgrupo_etareo='$row2[0]'";
+$sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' ";
+$result7 = mysqli_query($link,$sql7);
+$row7 = mysqli_fetch_array($result7);
+$cifra_masculino = $row7[0];
+?>
+             <?php echo "-".$cifra_masculino; ?>
+
+    <?php    
+    $numero2++;
+    if ($numero2 == $total2) { echo "";} else {echo ",";}
+
+} while ($row2 = mysqli_fetch_array($result2));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+</br>
+</br>
+
+<?php
+$numero3 = 0;
+$sql3 = " SELECT idgrupo_etareo, grupo_etareo FROM grupo_etareo ORDER BY idgrupo_etareo ";
+$result3 = mysqli_query($link,$sql3);
+$total3 = mysqli_num_rows($result3);
+ if ($row3 = mysqli_fetch_array($result3)){
+mysqli_field_seek($result3,0);
+while ($field3 = mysqli_fetch_field($result3)){
+} do {
+?>
+
+<?php
+$sql7 = " SELECT SUM(registro_enfermedad.cifra) FROM registro_enfermedad, notificacion_ep, genero  ";
+$sql7.= " WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND registro_enfermedad.idgenero=genero.idgenero ";
+$sql7.= " AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' AND registro_enfermedad.idgenero='1' AND registro_enfermedad.idgrupo_etareo='$row3[0]'";
+$sql7.= " AND registro_enfermedad.gestion='$gestion' AND notificacion_ep.estado='CONSOLIDADO' ";
+$result7 = mysqli_query($link,$sql7);
+$row7 = mysqli_fetch_array($result7);
+$cifra_femenino = $row7[0];
+?>
+             <?php echo $cifra_femenino; ?>
+
+    <?php    
+    $numero3++;
+    if ($numero3 == $total3) { echo "";} else {echo ",";}
+
+} while ($row3 = mysqli_fetch_array($result3));
+} else {
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+	</body>
+</html>
