69c7fea62bc21d77e17e43c3364e7c496f0d56a5
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-04-03 18:45:02 -0400

    avance al 3 de abril

--- inc.config.php
+++ inc.config.php
@@ -1,12 +1,7 @@
 <?php
 $link = mysqli_connect(
   'localhost',
-<<<<<<< HEAD
   'adminusr',
   '5afci+2023',
-=======
-  'root',
-  'root',
->>>>>>> develop_safci
   'safci_db'
 ) or die(mysqli_error($mysqli));
