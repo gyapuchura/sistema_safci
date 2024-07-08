8dd74a716f4e1ca007c43cd7fb31b9b641f73656
gonzito <gonzalo.yapuchura1982@gmail.com>
2024-07-07 21:53:15 -0400

    trabajo 6 y 7 de julio

---
+++ carpetas_familiares/determinante_funcionalidad_vivienda.php
@@ -0,0 +1,291 @@
+<?php include("../cabf.php");?>
+<?php include("../inc.config.php");?>
+<?php
+date_default_timezone_set('America/La_Paz');
+$fecha_ram	    = date("Ymd");
+$fecha 		    = date("Y-m-d");
+$gestion        = date("Y");
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>FUNCIONALIDAD VIVIENDA</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+
+		<script type="text/javascript">
+$(function () {
+    $('#tenencia_vivienda').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: 'a) TENENCIA DE LA VIVIENDA'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: ' % de las Familias',
+            data: [
+                <?php
+                    $sql0 = " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='16' ";
+                    $result0 = mysqli_query($link,$sql0);
+                    $row0 = mysqli_fetch_array($result0);
+                    $total = $row0[0];
+
+                    $numero = 0;
+                    $sql = " SELECT iditem_determinante_salud FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='16' GROUP BY iditem_determinante_salud ";
+                    $result = mysqli_query($link,$sql);
+                    $conteo_tipo = mysqli_num_rows($result);
+
+                    if ($row = mysqli_fetch_array($result)){
+                    mysqli_field_seek($result,0);
+                    while ($field = mysqli_fetch_field($result)){
+                    } do {
+
+                    $sql_t = " SELECT iditem_determinante_salud, item_determinante_salud FROM item_determinante_salud WHERE iditem_determinante_salud='$row[0]' ";
+                    $result_t = mysqli_query($link,$sql_t);
+                    $row_t = mysqli_fetch_array($result_t);
+
+                    $sql_c= " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='16' AND iditem_determinante_salud='$row[0]' ";
+                    $result_c = mysqli_query($link,$sql_c);
+                    $row_c = mysqli_fetch_array($result_c);
+                    $conteo = $row_c[0];
+
+                    $p_conteo   = ($conteo*100)/$total;
+                    $porcentaje    = number_format($p_conteo, 2, '.', '');
+
+                    ?>
+
+                    ['<?php echo $row_t[1];?>', <?php echo $porcentaje;?>]
+
+                        <?php
+                        $numero++;
+                        if ($numero == $conteo_tipo) {
+                        echo "";
+                        }
+                        else {
+                        echo ",";
+                        }
+                        
+                    } while ($row = mysqli_fetch_array($result));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>
+            ]
+        }]
+    });
+});
+		</script>
+
+<script type="text/javascript">
+$(function () {
+    $('#techo_vivienda').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: 'b) ÍNDICE DE HACINAMIENTO '
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: ' % de las Familias',
+            data: [
+                <?php
+                    $sql0 = " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='17' ";
+                    $result0 = mysqli_query($link,$sql0);
+                    $row0 = mysqli_fetch_array($result0);
+                    $total = $row0[0];
+
+                    $numero = 0;
+                    $sql = " SELECT iditem_determinante_salud FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='17' GROUP BY iditem_determinante_salud ";
+                    $result = mysqli_query($link,$sql);
+                    $conteo_tipo = mysqli_num_rows($result);
+
+                    if ($row = mysqli_fetch_array($result)){
+                    mysqli_field_seek($result,0);
+                    while ($field = mysqli_fetch_field($result)){
+                    } do {
+
+                    $sql_t = " SELECT iditem_determinante_salud, item_determinante_salud FROM item_determinante_salud WHERE iditem_determinante_salud='$row[0]' ";
+                    $result_t = mysqli_query($link,$sql_t);
+                    $row_t = mysqli_fetch_array($result_t);
+
+                    $sql_c= " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='17' AND iditem_determinante_salud='$row[0]' ";
+                    $result_c = mysqli_query($link,$sql_c);
+                    $row_c = mysqli_fetch_array($result_c);
+                    $conteo = $row_c[0];
+
+                    $p_conteo   = ($conteo*100)/$total;
+                    $porcentaje    = number_format($p_conteo, 2, '.', '');
+
+                    ?>
+
+                    ['<?php echo $row_t[1];?>', <?php echo $porcentaje;?>]
+
+                        <?php
+                        $numero++;
+                        if ($numero == $conteo_tipo) {
+                        echo "";
+                        }
+                        else {
+                        echo ",";
+                        }
+                        
+                    } while ($row = mysqli_fetch_array($result));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>
+            ]
+        }]
+    });
+});
+		</script>
+
+
+<script type="text/javascript">
+$(function () {
+    $('#paredes_vivienda').highcharts({
+        chart: {
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: 'c) TENENCIA DE ANIMALES EN LA VIVIENDA'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: '% de Familias',
+            data: [
+                <?php
+                    $sql0 = " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='18' ";
+                    $result0 = mysqli_query($link,$sql0);
+                    $row0 = mysqli_fetch_array($result0);
+                    $total = $row0[0];
+
+                    $numero = 0;
+                    $sql = " SELECT iditem_determinante_salud FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='18' GROUP BY iditem_determinante_salud ";
+                    $result = mysqli_query($link,$sql);
+                    $conteo_tipo = mysqli_num_rows($result);
+
+                    if ($row = mysqli_fetch_array($result)){
+                    mysqli_field_seek($result,0);
+                    while ($field = mysqli_fetch_field($result)){
+                    } do {
+
+                    $sql_t = " SELECT iditem_determinante_salud, item_determinante_salud FROM item_determinante_salud WHERE iditem_determinante_salud='$row[0]' ";
+                    $result_t = mysqli_query($link,$sql_t);
+                    $row_t = mysqli_fetch_array($result_t);
+
+                    $sql_c= " SELECT count(iddeterminante_salud_cf) FROM determinante_salud_cf WHERE iddeterminante_salud='3' AND idcat_determinante_salud='18' AND iditem_determinante_salud='$row[0]' ";
+                    $result_c = mysqli_query($link,$sql_c);
+                    $row_c = mysqli_fetch_array($result_c);
+                    $conteo = $row_c[0];
+
+                    $p_conteo   = ($conteo*100)/$total;
+                    $porcentaje    = number_format($p_conteo, 2, '.', '');
+
+                    ?>
+
+                    ['<?php echo $row_t[1];?>', <?php echo $porcentaje;?>]
+
+                        <?php
+                        $numero++;
+                        if ($numero == $conteo_tipo) {
+                        echo "";
+                        }
+                        else {
+                        echo ",";
+                        }
+                        
+                    } while ($row = mysqli_fetch_array($result));
+                    } else {
+                    /*
+                    Si no se encontraron resultados
+                    */
+                    }
+                    ?>
+            ]
+        }]
+    });
+});
+		</script>
+
+	</head>
+	<body>
+
+<script src="../js/highcharts.js"></script>
+<script src="../js/highcharts-3d.js"></script>
+<script src="../js/modules/exporting.js"></script>
+
+<div id="tenencia_vivienda" style="height: 350px"></div>
+<div id="techo_vivienda" style="height: 350px"></div>
+<div id="paredes_vivienda" style="height: 350px"></div>
+
+	</body>
+</html>
