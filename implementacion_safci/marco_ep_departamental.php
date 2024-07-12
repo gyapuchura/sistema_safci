9c36b8ed90f4dd739fd6fa9c43da68b25c67e8d0
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-07-12 16:12:15 -0400

    grafica ep

--- implementacion_safci/marco_ep_departamental.php
+++ implementacion_safci/marco_ep_departamental.php
@@ -174,39 +174,36 @@ Si no se encontraron resultados
 
 <?php
 
-$numero = 0;
-$sql = " SELECT semana_ep FROM notificacion_ep WHERE gestion ='$gestion' AND estado='CONSOLIDADO' GROUP BY semana_ep ORDER BY semana_ep ";
-$result = mysqli_query($link,$sql);
-
-$total = mysqli_num_rows($result);
-
-if ($row = mysqli_fetch_array($result)){
-
-mysqli_field_seek($result,0);
-while ($field = mysqli_fetch_field($result)){
+$numero_r = 0;
+$sql_r = " SELECT semana_ep FROM notificacion_ep WHERE gestion ='$gestion' AND estado='CONSOLIDADO' GROUP BY semana_ep ORDER BY semana_ep ";
+$result_r = mysqli_query($link,$sql_r);
+$total_r = mysqli_num_rows($result_r);
+if ($row_r = mysqli_fetch_array($result_r)){
+mysqli_field_seek($result_r,0);
+while ($field_r = mysqli_fetch_field($result_r)){
 } do {
 ?>
 
 <?php
-$sql7 = " SELECT count(seguimiento_ep.idseguimiento_ep) FROM seguimiento_ep, notificacion_ep ";
-$sql7.= " WHERE seguimiento_ep.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND seguimiento_ep.idestado_paciente='2' ";
-$sql7.= " AND seguimiento_ep.idsospecha_diag='$idsospecha_diag_deptal' AND seguimiento_ep.idsemana_ep='$row[0]' AND notificacion_ep.iddepartamento='$iddepartamento_ep' ";
-$result7 = mysqli_query($link,$sql7);
-$row7 = mysqli_fetch_array($result7);
-$recuperados = $row7[0];
+$sql8 = " SELECT count(seguimiento_ep.idseguimiento_ep) FROM seguimiento_ep, notificacion_ep ";
+$sql8.= " WHERE seguimiento_ep.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND seguimiento_ep.idestado_paciente='2' ";
+$sql8.= " AND seguimiento_ep.idsospecha_diag='$idsospecha_diag_deptal' AND seguimiento_ep.idsemana_ep='$row_r[0]' AND notificacion_ep.iddepartamento='$iddepartamento_ep' ";
+$result8 = mysqli_query($link,$sql8);
+$row8 = mysqli_fetch_array($result8);
+$recuperados = $row8[0];
 ?>
 <?php echo $recuperados; ?>
 
 <?php
-$numero++;
-if ($numero == $total) {
+$numero_r++;
+if ($numero_r == $total_r) {
 echo "";
 }
 else {
 echo ",";
 }
 
-} while ($row = mysqli_fetch_array($result));
+} while ($row_r = mysqli_fetch_array($result_r));
 
 
 } else {
@@ -241,40 +238,40 @@ Si no se encontraron resultados
 		     <!--- <td width="106" style="color: #2D56CF; font-size: 12px; font-family: Arial; text-align: center;">F302A</td>  --->
 	        </tr>
             <?php
-    $numero=1; 
-    $sql =" SELECT notificacion_ep.idmunicipio, municipios.municipio FROM notificacion_ep, registro_enfermedad, municipios ";
-    $sql.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND notificacion_ep.idmunicipio=municipios.idmunicipio AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_deptal' ";
-    $sql.=" AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.gestion='$gestion' AND notificacion_ep.iddepartamento='$iddepartamento_ep' GROUP BY notificacion_ep.idmunicipio ORDER BY municipios.municipio";
-    $result = mysqli_query($link,$sql);
-    if ($row = mysqli_fetch_array($result)){
-    mysqli_field_seek($result,0);           
-    while ($field = mysqli_fetch_field($result)){
+    $numero_m=1; 
+    $sql_m =" SELECT notificacion_ep.idmunicipio, municipios.municipio FROM notificacion_ep, registro_enfermedad, municipios ";
+    $sql_m.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND notificacion_ep.idmunicipio=municipios.idmunicipio AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_deptal' ";
+    $sql_m.=" AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.gestion='$gestion' AND notificacion_ep.iddepartamento='$iddepartamento_ep' GROUP BY notificacion_ep.idmunicipio ORDER BY municipios.municipio";
+    $result_m = mysqli_query($link,$sql_m);
+    if ($row_m = mysqli_fetch_array($result_m)){
+    mysqli_field_seek($result_m,0);           
+    while ($field_m = mysqli_fetch_field($result_m)){
     } do {
 
-        $sql_c =" SELECT SUM(registro_enfermedad.cifra) FROM notificacion_ep, registro_enfermedad ";
-        $sql_c.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep ";
-        $sql_c.=" AND notificacion_ep.estado='CONSOLIDADO' AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_deptal' ";
-        $sql_c.=" AND notificacion_ep.gestion='$gestion' AND notificacion_ep.idmunicipio='$row[0]' ";
-        $result_c = mysqli_query($link,$sql_c);
-        $row_c = mysqli_fetch_array($result_c);
+        $sql_ci =" SELECT SUM(registro_enfermedad.cifra) FROM notificacion_ep, registro_enfermedad ";
+        $sql_ci.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep ";
+        $sql_ci.=" AND notificacion_ep.estado='CONSOLIDADO' AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_deptal' ";
+        $sql_ci.=" AND notificacion_ep.gestion='$gestion' AND notificacion_ep.idmunicipio='$row_m[0]' ";
+        $result_ci = mysqli_query($link,$sql_ci);
+        $row_ci = mysqli_fetch_array($result_ci);
     ?>
 		    <tr>
-		      <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $numero;?></td>
-		      <td style="font-size: 12px; font-family: Arial;"><?php echo $row[1];?></td>
-              <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $row_c[0];?></td>
+		      <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $numero_m;?></td>
+		      <td style="font-size: 12px; font-family: Arial;"><?php echo $row_m[1];?></td>
+              <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $row_ci[0];?></td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
-              <a href="marco_ep_municipal.php?idsospecha_diag_mun=<?php echo $idsospecha_diag_deptal;?>&idmunicipio=<?php echo $row[0];?>" target="_blank" class="Estilo12" style="font-size: 12px; font-family: Arial;" onClick="window.open(this.href, this.target, 'width=1000,height=600,scrollbars=YES,top=60,left=400'); return false;">REPORTE</a>
+              <a href="marco_ep_municipal.php?idsospecha_diag_mun=<?php echo $idsospecha_diag_deptal;?>&idmunicipio=<?php echo $row_m[0];?>" target="_blank" class="Estilo12" style="font-size: 12px; font-family: Arial;" onClick="window.open(this.href, this.target, 'width=1000,height=600,scrollbars=YES,top=60,left=400'); return false;">REPORTE</a>
               </td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
-              <a href="piramide_sospechas_mun.php?idsospecha_diag_mun=<?php echo $idsospecha_diag_deptal;?>&idmunicipio=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=400,scrollbars=YES,top=50,left=300'); return false;">             
+              <a href="piramide_sospechas_mun.php?idsospecha_diag_mun=<?php echo $idsospecha_diag_deptal;?>&idmunicipio=<?php echo $row_m[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=400,scrollbars=YES,top=50,left=300'); return false;">             
               GRUPOS</a>
               </td>
 		     <!--- <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">&nbsp;</td> --->
 	        </tr>
             <?php
-        $numero=$numero+1;
+        $numero_m=$numero_m+1;
         }
-        while ($row = mysqli_fetch_array($result));
+        while ($row_m = mysqli_fetch_array($result_m));
         } else {
         }
         ?>
