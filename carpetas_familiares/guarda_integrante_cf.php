69616a79f4ddc85e3e4f20b0a7c51b8da7ed2766
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-07-15 16:40:31 -0400

    bug int

--- carpetas_familiares/guarda_integrante_cf.php
+++ carpetas_familiares/guarda_integrante_cf.php
@@ -36,10 +36,72 @@ $fecha_nacimiento = $row_n[5];
     $ano=($ano-1);}       
     $edad=($ano-$anonaz);  
 
+if ($edad <= 4) {
+    $idgrupo_etareo_cf = '1';
+    } else {
+    if ($edad <= 9) {
+    $idgrupo_etareo_cf = '2';
+    } else {
+    if ($edad <= 14) {
+        $idgrupo_etareo_cf = '3';
+    } else { 
+        if ($edad <= 19) {
+        $idgrupo_etareo_cf = '4';
+    } else { 
+        if ($edad <= 24) {
+            $idgrupo_etareo_cf = '5';
+        } else { 
+            if ($edad <= 29) {
+                $idgrupo_etareo_cf = '6';
+            } else { 
+                if ($edad <= 34) {
+                    $idgrupo_etareo_cf = '7';
+                } else { 
+                    if ($edad <= 39) {
+                        $idgrupo_etareo_cf = '8';
+                    } else { 
+                        if ($edad <= 44) {
+                            $idgrupo_etareo_cf = '9';
+                        } else { 
+                            if ($edad <= 49) {
+                                $idgrupo_etareo_cf = '10';
+                            } else { 
+                                if ($edad <= 54) {
+                                    $idgrupo_etareo_cf = '11';
+                                } else { 
+                                    if ($edad <= 59) {
+                                        $idgrupo_etareo_cf = '12';
+                                    } else { 
+                                        if ($edad <= 64) {
+                                            $idgrupo_etareo_cf = '13';
+                                        } else { 
+                                            if ($edad <= 69) {
+                                                $idgrupo_etareo_cf = '14';
+                                            } else { 
+                                                if ($edad <= 74) {
+                                                    $idgrupo_etareo_cf = '15';
+                                                } else { 
+                                                    if ($edad <= 79) {
+                                                        $idgrupo_etareo_cf = '16';
+                                                    } else { 
+                                                        if ($edad <= 84) {
+                                                            $idgrupo_etareo_cf = '17';
+                                                        } else { 
+                                                            if ($edad <= 89) {
+                                                                $idgrupo_etareo_cf = '18';
+                                                            } else { 
+                                                                if ($edad <= 94) {
+                                                                    $idgrupo_etareo_cf = '19';
+                                                                } else { 
+                                                                    if ($edad >= 95) {
+                                                                        $idgrupo_etareo_cf = '20';
+                                                                    } else { 
+    }}}}} }}}}} }}}}} }}}}}
+
 /*********** Crear registros para fichas epidemiologicas (BEGIN) *************/
 
-    $sql2 = " INSERT INTO integrante_cf (idcarpeta_familiar, idnombre, edad, idparentesco, idnacion, fecha_registro, hora_registro, idusuario) ";
-    $sql2.= " VALUES ('$idcarpeta_familiar_ss','$idnombre_integrante_ss','$edad','$idparentesco_ss','$idnacion_ss','$fecha','$hora','$idusuario_ss') ";
+    $sql2 = " INSERT INTO integrante_cf (idcarpeta_familiar, idnombre, edad, idgrupo_etareo_cf, idparentesco, idnacion, fecha_registro, hora_registro, idusuario) ";
+    $sql2.= " VALUES ('$idcarpeta_familiar_ss','$idnombre_integrante_ss','$edad','$idgrupo_etareo_cf','$idparentesco_ss','$idnacion_ss','$fecha','$hora','$idusuario_ss') ";
     $result2 = mysqli_query($link,$sql2);   
     $idnombre_paciente = mysqli_insert_id($link);
 
