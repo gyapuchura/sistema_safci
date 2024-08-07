4a5e08526dac967b99b4e546aa43527443884679
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-08-07 18:30:48 -0400

    bug eess garficas

--- carpetas_familiares/establecimientos_cf.php
+++ carpetas_familiares/establecimientos_cf.php
@@ -5,9 +5,8 @@ $idmunicipio = $_POST["municipio_salud"];
 <option value="">Elegir Establecimiento de Salud</option>
 <?php
 $numero3 = 1;
-$sql3 = " SELECT ubicacion_cf.idestablecimiento_salud, establecimiento_salud.establecimiento_salud ";
-$sql3.= " FROM ubicacion_cf, establecimiento_salud  ";
-$sql3.= " WHERE ubicacion_cf.idestablecimiento_salud=establecimiento_salud.idestablecimiento_salud ";
+$sql3 = " SELECT ubicacion_cf.idestablecimiento_salud, establecimiento_salud.establecimiento_salud FROM ubicacion_cf, establecimiento_salud, carpeta_familiar ";
+$sql3.= " WHERE ubicacion_cf.idestablecimiento_salud=establecimiento_salud.idestablecimiento_salud AND ubicacion_cf.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND carpeta_familiar.estado='CONSOLIDADO' ";
 $sql3.= " AND ubicacion_cf.idmunicipio='$idmunicipio' GROUP BY ubicacion_cf.idestablecimiento_salud ORDER BY establecimiento_salud.establecimiento_salud ";
 $result3 = mysqli_query($link,$sql3);
 if ($row3 = mysqli_fetch_array($result3)){
