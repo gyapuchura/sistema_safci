0a57cc44e114efff6de6335949b60329444c4666
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-09-09 18:06:54 -0400

    determinantes de la salud

--- carpetas_familiares/factor_determinante.php
+++ carpetas_familiares/factor_determinante.php
@@ -1,6 +1,7 @@
 <?php include("../cabf.php"); ?>
 <?php include("../inc.config.php");
 $idcarpeta_familiar_ss  = $_SESSION['idcarpeta_familiar_ss'];
+
 $idcat_determinante_salud = $_POST["cat_determinante_salud"];
 ?>
 <?php
@@ -11,7 +12,7 @@ if ($idcat_determinante_salud == '17') { ?>
         </div>
         <div class="col-sm-2">   
             <?php 
-                    $sql1 = "SELECT idintegrante_cf, idcarpeta_familiar FROM integrante_cf WHERE idcarpeta_familiar='$idcarpeta_familiar_ss' ";
+                    $sql1 = "SELECT idintegrante_cf, idcarpeta_familiar FROM integrante_cf WHERE idcarpeta_familiar='$idcarpeta_familiar_ss' AND estado='CONSOLIDADO'";
                     $result1 = mysqli_query($link,$sql1);
                     $integrantes = mysqli_num_rows($result1);
                     echo $integrantes;
@@ -19,7 +20,7 @@ if ($idcat_determinante_salud == '17') { ?>
             <input type="hidden" name="integrantes" value="<?php echo $integrantes;?>">
         </div>
         <div class="col-sm-4">
-        <h6 class="text-info">CUANTOS CUARTOS O HABITACIONES DE LA VIVIENDA OCUPA PARA DORMIR</h6>
+        <h6 class="text-info">CUANTOS CUARTOS O HABITACIONES DE LA VIVIENDA OCUPA PARA DORMIR?</h6>
         </div>
         <div class="col-sm-2">   
         <input type="number" class="form-control" name="habitaciones" required>
@@ -112,22 +113,24 @@ if ($idcat_determinante_salud == '17') { ?>
     <h6 class="text-info">FACTOR DETERMINANTE</h6>
     </div>
     <div class="col-sm-9">   
-            <select name="iditem_determinante_salud" id="iditem_determinante_salud" class="form-control" required>
-            <option value="">Elegir</option>
+
+
             <?php
             $sql2 = "SELECT iditem_determinante_salud, item_determinante_salud, valor FROM item_determinante_salud WHERE idcat_determinante_salud='$idcat_determinante_salud'";
             $result2 = mysqli_query($link,$sql2);
             if ($row2 = mysqli_fetch_array($result2)){
             mysqli_field_seek($result2,0);
             while ($field2 = mysqli_fetch_field($result2)){
-            } do {
-            echo "<option value=".$row2[0].">".$row2[1]."</option>";
-            } while ($row2 = mysqli_fetch_array($result2));
+            } do { ?>
+
+               <h6><?php echo $row2[1];?> -> <input type="radio" name="iditem_determinante_salud" value="<?php echo $row2[0];?>" required></h6>
+
+           <?php } while ($row2 = mysqli_fetch_array($result2));
             } else {
             echo "No se encontraron resultados!";
             }
             ?> 
-                    </select>
+                    
                 </div>
             </div>
             
