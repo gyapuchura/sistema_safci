3ed2de067c17752fff7c15596c7ff3fbca57af18
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-09-17 17:58:23 -0400

    desconsolida integrantes de la familia

--- carpetas_familiares/elimina_integrante_cf.php
+++ carpetas_familiares/elimina_integrante_cf.php
@@ -15,13 +15,36 @@ $idcarpeta_familiar_ss = $_SESSION['idcarpeta_familiar_ss'];
 
 /*********** ENVIO DATOS PARA ELIMINAR INTEGRANTE *************/
 
-$idintegrante_cf        = $_POST['idintegrante_cf'];
+$idintegrante_cf = $_POST['idintegrante_cf'];
 
 /* BORRAMOS EL REGISTRO*/
 
-$sql = " DELETE FROM integrante_cf WHERE idintegrante_cf='$idintegrante_cf'";
+$sql = " DELETE FROM integrante_cf WHERE idintegrante_cf='$idintegrante_cf' ";
 $result = mysqli_query($link,$sql);
 
+$sql = " DELETE FROM integrante_ap_sano WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_factor_riesgo WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_morbilidad WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_discapacidad WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_subsector_salud WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_beneficiario WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_tradicional WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
+
+$sql = " DELETE FROM integrante_defuncion WHERE idintegrante_cf='$idintegrante_cf' ";
+$result = mysqli_query($link,$sql);
 
 header("Location:integrantes_cf.php");
 
---
+++ carpetas_familiares/mensaje_desconsolida_integrantes.php
@@ -0,0 +1,155 @@
+<?php include("../cabf.php"); ?>
+<?php include("../inc.config.php"); ?>
+<?php
+date_default_timezone_set('America/La_Paz');
+$fecha_ram				= date("Ymd");
+$fecha 					= date("Y-m-d");
+$gestion = date("Y");
+
+$idusuario_ss  =  $_SESSION['idusuario_ss'];
+$idnombre_ss   =  $_SESSION['idnombre_ss'];
+$perfil_ss     =  $_SESSION['perfil_ss'];
+
+$idcarpeta_familiar_ss = $_SESSION['idcarpeta_familiar_ss'];
+
+$sql_cf =" SELECT carpeta_familiar.idcarpeta_familiar, carpeta_familiar.codigo, ubicacion_cf.iddepartamento, ubicacion_cf.idred_salud, ubicacion_cf.idmunicipio, ubicacion_cf.idestablecimiento_salud, ";
+$sql_cf.=" ubicacion_cf.idarea_influencia, carpeta_familiar.fecha_apertura, carpeta_familiar.familia, ubicacion_cf.avenida_calle, ubicacion_cf.no_puerta, ubicacion_cf.nombre_edificio, ";
+$sql_cf.=" ubicacion_cf.latitud, ubicacion_cf.longitud, ubicacion_cf.altura, carpeta_familiar.fecha_registro, carpeta_familiar.hora_registro ";
+$sql_cf.=" FROM carpeta_familiar, ubicacion_cf WHERE ubicacion_cf.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar ";
+$sql_cf.=" AND ubicacion_cf.ubicacion_actual='SI' AND carpeta_familiar.idcarpeta_familiar='$idcarpeta_familiar_ss' ";
+$result_cf=mysqli_query($link,$sql_cf);
+$row_cf=mysqli_fetch_array($result_cf);
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SISTEMA MEDI-SAFCI</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template -->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/jquery-ui.min.css">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include("../menu.php");?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include("../top_bar.php"); ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+  
+                <body class="bg-gradient-primary">
+
+    <div class="container">
+    </br>
+        <div class="card o-hidden border-0 shadow-lg my-2">
+            <div class="card-body p-0">
+<!-- BEGIN aqui va el TITULO de la pagina ---->
+                <div class="row">
+                    <div class="col-lg-12">
+                    <div class="p-3">               
+                    <div class="text-center">   
+                    
+                    <hr>                     
+                    <h4 class="text-success">LA LISTA DE INTEGRANTES DE LA CARPETA FAMILIAR</h4>
+                    <h4 class="text-secundary"><?php echo $row_cf[1]?></h4>
+                    <h4 class="text-success">DE LA FAMILIA : </h4>
+                    <h4 class="text-secundary"><?php echo $row_cf[8]?></h4>
+                    <h4 class="text-danger">HA SIDO DESCONSOLIDADA!</h4>
+                    <h4 class="text-danger">IMPORTANTE</h4>
+                    <h4 class="text-secundary">AHORA PODRA ELIMINAR INTEGRANTES</h4>
+                    <h4 class="text-sucsecundarycess">Y REALIZAR LOS AJUSTES CORRESPONDIENTES</h4>
+                    </br>
+                    <a href="integrantes_cf.php"><h6>IR A INTEGRANTES DE LA FAMILIA -></h6></a>
+                    </div>
+<!-- END Del TITULO de la pagina ---->
+
+<!-- BEGIN aqui va el comntenido de la pagina ---->
+
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                    </div>
+                    <div class="col-sm-6">
+                    </div>
+                </div>                  
+                    
+<!-- END aqui va el comntenido de la pagina ---->
+                </div>
+               
+                <div class="text-center">
+                <hr>
+                    <a class="small" href="#">PROGRAMA SAFCI - MI SALUD</a>
+                </div>
+                <div class="text-center">
+                    <a class="small" href="#">Ministerio de Salud y Deportes</a>
+                <hr>
+                </div>
+               
+            </div>   
+        </div> 
+    </div>
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">¿ESTA SEGURO DE SALIR?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Seleccione la opcion Salir para cerrar sesion tendrá que volver a introducir su password.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="../salir.php">Salir de Sistema</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- scripts para calendario -->
+   
+</body>
+
+</html>
