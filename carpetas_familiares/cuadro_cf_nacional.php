f007ab28c5a5e0a9ccbbda0e1df46f0c9c7241e1
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2024-11-15 16:41:31 -0400

    piramide ap sanos

--- carpetas_familiares/cuadro_cf_nacional.php
+++ carpetas_familiares/cuadro_cf_nacional.php
@@ -22,8 +22,10 @@ $gestion    = date("Y");
 		  <tbody>
 		    <tr>
 		      <td width="37" style="font-family: Arial; font-size: 12px; color: #2D56CF; text-align: center;">N°</td>
-              <td width="199" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">GRUPO DE SALUD</td>
-              <td width="110" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">N° DE INTEGRANTES</td>
+              <td width="120" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">GRUPO DE SALUD</td>
+              <td width="80" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">N° DE INTEGRANTES</td>
+              <td width="100" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">DEPARTAMENTOS</td>
+              <td width="100" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">ETÁREO</td>
 		     <!--- <td width="106" style="color: #2D56CF; font-size: 12px; font-family: Arial; text-align: center;">F302A</td>  --->
 	        </tr>
 
@@ -32,15 +34,21 @@ $gestion    = date("Y");
               <td style="font-size: 12px; font-family: Arial;">APARENTEMENTE SANOS</td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
                 <?php
-                $sql_a =" SELECT COUNT(integrante_ap_sano.idintegrante_ap_sano) FROM integrante_ap_sano, integrante_cf, carpeta_familiar  ";
-                $sql_a.=" WHERE integrante_ap_sano.idintegrante_cf=integrante_cf.idintegrante_cf AND integrante_cf.estado='CONSOLIDADO' AND ";
-                $sql_a.=" integrante_ap_sano.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar";
-                $sql_a.=" AND carpeta_familiar.estado='CONSOLIDADO' ";
+                $sql_a =" SELECT COUNT(integrante_ap_sano.idintegrante_ap_sano) FROM integrante_ap_sano, carpeta_familiar WHERE carpeta_familiar.estado='CONSOLIDADO'  ";
+                $sql_a.=" AND integrante_ap_sano.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar  ";
                 $result_a = mysqli_query($link,$sql_a);
                 $row_a = mysqli_fetch_array($result_a);
                 $aparentemente_sano = $row_a[0];
                 echo $aparentemente_sano;
                 ?>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="aparentemente_sanos_deptos.php" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=450,scrollbars=YES,top=60,left=500'); return false;">             
+              DEPARTAMENTOS</a>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="piramide_aparentemente_sanos_nal.php" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=800,height=800,scrollbars=YES,top=60,left=500'); return false;">             
+              VER PIRAMIDE</a>
               </td>
 	        </tr>
             <tr>
@@ -48,15 +56,21 @@ $gestion    = date("Y");
               <td style="font-size: 12px; font-family: Arial;">CON FACTORES DE RIESGO</td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
                 <?php
-                $sql_b =" SELECT COUNT(integrante_factor_riesgo.idintegrante_factor_riesgo) FROM integrante_factor_riesgo, integrante_cf, carpeta_familiar  ";
-                $sql_b.=" WHERE integrante_factor_riesgo.idintegrante_cf=integrante_cf.idintegrante_cf AND integrante_cf.estado='CONSOLIDADO' AND ";
-                $sql_b.=" integrante_factor_riesgo.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar";
-                $sql_b.=" AND carpeta_familiar.estado='CONSOLIDADO' ";
+                $sql_b =" SELECT COUNT(integrante_factor_riesgo.idintegrante_factor_riesgo) FROM integrante_factor_riesgo, carpeta_familiar  ";
+                $sql_b.=" WHERE integrante_factor_riesgo.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND carpeta_familiar.estado='CONSOLIDADO' ";
                 $result_b = mysqli_query($link,$sql_b);
                 $row_b = mysqli_fetch_array($result_b);
                 $factor_riesgo = $row_b[0];
                 echo $factor_riesgo;
                 ?>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="factores_riesgo_deptos.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=450,scrollbars=YES,top=60,left=500'); return false;">             
+              DEPARTAMENTOS</a>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="piramide_factores_riesgo_nal.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=800,height=800,scrollbars=YES,top=60,left=500'); return false;">             
+              VER PIRAMIDE</a>
               </td>
 	        </tr>
             <tr>
@@ -64,16 +78,21 @@ $gestion    = date("Y");
               <td style="font-size: 12px; font-family: Arial;">CON MORBILIDAD</td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
                 <?php
-                $sql_c =" SELECT COUNT(integrante_morbilidad.idintegrante_morbilidad) FROM integrante_morbilidad, integrante_cf, carpeta_familiar  ";
-                $sql_c.=" WHERE integrante_morbilidad.idintegrante_cf=integrante_cf.idintegrante_cf AND integrante_cf.estado='CONSOLIDADO' AND ";
-                $sql_c.=" integrante_morbilidad.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar";
-                $sql_c.=" AND carpeta_familiar.estado='CONSOLIDADO' ";
-
+                $sql_c =" SELECT COUNT(integrante_morbilidad.idintegrante_morbilidad) FROM integrante_morbilidad, carpeta_familiar  ";
+                $sql_c.=" WHERE integrante_morbilidad.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND carpeta_familiar.estado='CONSOLIDADO' ";
                 $result_c = mysqli_query($link,$sql_c);
                 $row_c = mysqli_fetch_array($result_c);
                 $morbilidad =$row_c[0];
                 echo $morbilidad;
                 ?>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="factores_riesgo_deptos.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=450,scrollbars=YES,top=60,left=500'); return false;">             
+              DEPARTAMENTOS</a>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="piramide_factores_riesgo_nal.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=800,height=800,scrollbars=YES,top=60,left=500'); return false;">             
+              VER PIRAMIDE</a>
               </td>
 	        </tr>
             <tr>
@@ -81,15 +100,21 @@ $gestion    = date("Y");
               <td style="font-size: 12px; font-family: Arial;">CON DISCAPACIDAD</td>
 		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
                 <?php
-                $sql_d =" SELECT COUNT(integrante_discapacidad.idintegrante_discapacidad) FROM integrante_discapacidad, integrante_cf, carpeta_familiar  ";
-                $sql_d.=" WHERE integrante_discapacidad.idintegrante_cf=integrante_cf.idintegrante_cf AND integrante_cf.estado='CONSOLIDADO' AND  ";
-                $sql_d.=" integrante_discapacidad.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar";
-                $sql_d.=" AND carpeta_familiar.estado='CONSOLIDADO' ";
+                $sql_d =" SELECT COUNT(integrante_discapacidad.idintegrante_discapacidad) FROM integrante_discapacidad, carpeta_familiar  ";
+                $sql_d.=" WHERE integrante_discapacidad.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND carpeta_familiar.estado='CONSOLIDADO' ";
                 $result_d = mysqli_query($link,$sql_d);
                 $row_d = mysqli_fetch_array($result_d);
                 $discapacidad = $row_d[0];
                 echo $discapacidad;
                 ?>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="factores_riesgo_deptos.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=450,scrollbars=YES,top=60,left=500'); return false;">             
+              DEPARTAMENTOS</a>
+              </td>
+              <td style="font-size: 12px; font-family: Arial;">
+              <a href="piramide_factores_riesgo_nal.php?idfactor_riesgo_cf=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=800,height=800,scrollbars=YES,top=60,left=500'); return false;">             
+              VER PIRAMIDE</a>
               </td>
 	        </tr>
 
