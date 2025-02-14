0392f159b0662e0259161834904c88afd22a4200
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2025-02-14 17:10:53 -0400

    epidemiologia cuadros

--- implementacion_safci/marco_ep_nacional.php
+++ implementacion_safci/marco_ep_nacional.php
@@ -226,11 +226,59 @@ Si no se encontraron resultados
 <script src="../js/modules/exporting.js"></script>
 <div id="container" style="min-width: 300px; height: 350px; margin: 0 auto"></div>
 
-<h2 style="text-align: center; font-family: Arial; font-size: 14px; color: #2D56CF;"> <?php echo mb_strtoupper($row_sos[1]);?> A NIVEL NACIONAL</h2>
+<h2 style="text-align: center; font-family: Arial; font-size: 14px; color: #2D56CF;">DEPARTAMENTOS CON SOSPECHAS DE <?php echo mb_strtoupper($row_sos[1]);?></h2>
+		<table width="700" border="1" align="center" cellspacing="0">
+		  <tbody>
+		    <tr>
+		      <td width="37" style="font-family: Arial; font-size: 12px; color: #2D56CF; text-align: center;">N°</td>
+              <td width="199" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">DEPARTAMENTO</td>
+              <td width="110" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">N° SOSPECHAS</td>
+		      <td width="101" style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">REPORTE </td>
+		      <td width="121" style="color: #2D56CF; font-size: 12px; font-family: Arial; text-align: center;">GRUPOS ETAREOS</td>
+		     <!--- <td width="106" style="color: #2D56CF; font-size: 12px; font-family: Arial; text-align: center;">F302A</td>  --->
+	        </tr>
+            <?php
+    $numero=1; 
+    $sql =" SELECT notificacion_ep.iddepartamento, departamento.departamento FROM notificacion_ep, registro_enfermedad,  departamento ";
+    $sql.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep AND notificacion_ep.iddepartamento=departamento.iddepartamento AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal'  ";
+    $sql.=" AND notificacion_ep.estado='CONSOLIDADO' AND notificacion_ep.gestion='$gestion' GROUP BY notificacion_ep.iddepartamento ORDER BY notificacion_ep.iddepartamento ";
+    $result = mysqli_query($link,$sql);
+    if ($row = mysqli_fetch_array($result)){
+    mysqli_field_seek($result,0);           
+    while ($field = mysqli_fetch_field($result)){
+    } do {
 
-<h2 style="text-align: center; font-family: Arial; font-size: 14px; color: #2D56CF;">
-<a href="cuadro_ep_departamental.php?sospecha_diag_nal=<?php echo $idsospecha_diag_nal;?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=800,height=300,scrollbars=YES,top=60,left=400'); return false;">             
-MOSTRAR CUADRO DEPARTAMENTAL</a></h2>
+        $sql_c =" SELECT SUM(registro_enfermedad.cifra) FROM notificacion_ep, registro_enfermedad ";
+        $sql_c.=" WHERE registro_enfermedad.idnotificacion_ep=notificacion_ep.idnotificacion_ep ";
+        $sql_c.=" AND notificacion_ep.estado='CONSOLIDADO' AND registro_enfermedad.idsospecha_diag='$idsospecha_diag_nal' ";
+        $sql_c.=" AND notificacion_ep.gestion='$gestion' AND notificacion_ep.iddepartamento='$row[0]' ";
+        $result_c = mysqli_query($link,$sql_c);
+        $row_c = mysqli_fetch_array($result_c);
+
+    ?>
+		    <tr>
+		      <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $numero;?></td>
+              <td style="font-size: 12px; font-family: Arial;"><?php echo $row[1];?></td>
+              <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $row_c[0];?></td>
+		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
+              <a href="marco_ep_departamental.php?sospecha_diag_deptal=<?php echo $idsospecha_diag_nal;?>&departamento_ep=<?php echo $row[0];?>" target="_blank" class="Estilo12" style="font-size: 12px; font-family: Arial;" onClick="window.open(this.href, this.target, 'width=1000,height=450,scrollbars=YES,top=60,left=400'); return false;">REPORTE</a>
+              </td>
+		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
+              <a href="piramide_sospechas_deptal.php?sospecha_diag_deptal=<?php echo $idsospecha_diag_nal;?>&departamento_ep=<?php echo $row[0];?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=400,scrollbars=YES,top=50,left=300'); return false;">             
+              GRUPOS</a> 
+              </td>
+		     <!--- <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">&nbsp;</td> --->
+	        </tr>
+            <?php
+        $numero=$numero+1;
+        }
+        while ($row = mysqli_fetch_array($result));
+        } else {
+        }
+        ?>
+	      </tbody>
+    </table>
+
 
 </body>
 </html>
