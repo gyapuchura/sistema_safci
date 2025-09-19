681a31eb76db081383997a48e4ac423cce66e043
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2025-09-19 17:03:55 -0400

    sala situacional municipio -1

--- menu.php
+++ menu.php
@@ -208,6 +208,7 @@
     } do {	?>   
 
             <a class="collapse-item" href="../sala_situacional/sala_situacional_est.php">SALA SITUACIONAL</br>ESTABLECIMIENTO</br> DE SALUD</a>
+            <a class="collapse-item" href="../sala_situacional/sala_situacional_mun.php">SALA SITUACIONAL</br>MUNICIPIO</a>
             <a class="collapse-item" href="../sala_situacional/sala_personal_safci.php">PERSONAL SAFCI</a>
             <a class="collapse-item" href="../sala_situacional/sala_establecimientos.php">ESTABLECIMIENTOS</a>
             <a class="collapse-item" href="../sala_situacional/sala_areas_influencia.php">ÁREAS DE INFLUENCIA</a>
