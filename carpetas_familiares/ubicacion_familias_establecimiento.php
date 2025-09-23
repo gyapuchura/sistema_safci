3e4a73e72a69615fe998c05c0f740d3ecb2327f5
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2025-09-23 16:11:54 -0400

    sala situacional v1

--- carpetas_familiares/ubicacion_familias_establecimiento.php
+++ carpetas_familiares/ubicacion_familias_establecimiento.php
@@ -10,14 +10,14 @@ $idestablecimiento_salud = $_GET['idestablecimiento_salud'];
 
 $sql1 = " SELECT carpeta_familiar.idcarpeta_familiar, establecimiento_salud.establecimiento_salud, ubicacion_cf.latitud, ubicacion_cf.longitud  ";
 $sql1.= " FROM carpeta_familiar,  ubicacion_cf, establecimiento_salud WHERE ubicacion_cf.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar ";
-$sql1.= " AND ubicacion_cf.idestablecimiento_salud=establecimiento_salud.idestablecimiento_salud ";
-$sql1.= " AND carpeta_familiar.estado='CONSOLIDADO' AND  ubicacion_cf.idestablecimiento_salud='$idestablecimiento_salud' LIMIT 1 ";
+$sql1.= " AND carpeta_familiar.idestablecimiento_salud=establecimiento_salud.idestablecimiento_salud ";
+$sql1.= " AND carpeta_familiar.estado='CONSOLIDADO' AND  carpeta_familiar.idestablecimiento_salud='$idestablecimiento_salud' LIMIT 1 ";
 $result1 = mysqli_query($link,$sql1);
 $row1 = mysqli_fetch_array($result1);
 
 $latitud_c  = $row1[2];
 $longitud_c = $row1[3];
-$zoom_c     = "16";
+$zoom_c     = "18";
 
 ?>
 
@@ -46,33 +46,27 @@ $zoom_c     = "16";
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-
-        <?php
+<?php
 /****** Areas de influencia del Establecimiento de salud *********/
 $numero4 = 0;
 $sql4 = " SELECT carpeta_familiar.idcarpeta_familiar, carpeta_familiar.familia, tipo_area_influencia.tipo_area_influencia, area_influencia.area_influencia,   ";
 $sql4.= " ubicacion_cf.avenida_calle, ubicacion_cf.no_puerta, ubicacion_cf.latitud, ubicacion_cf.longitud   ";
 $sql4.= " FROM carpeta_familiar, area_influencia, tipo_area_influencia, ubicacion_cf WHERE ubicacion_cf.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND  ";
 $sql4.= " carpeta_familiar.idarea_influencia=area_influencia.idarea_influencia AND area_influencia.idtipo_area_influencia=tipo_area_influencia.idtipo_area_influencia  ";
-$sql4.= " AND carpeta_familiar.estado='CONSOLIDADO' AND ubicacion_cf.ubicacion_actual='SI' AND carpeta_familiar.idestablecimiento_salud='$idestablecimiento_salud'  ";
+$sql4.= " AND carpeta_familiar.estado='CONSOLIDADO' AND ubicacion_cf.ubicacion_actual='SI' AND carpeta_familiar.idestablecimiento_salud='$idestablecimiento_salud' ORDER BY carpeta_familiar.idcarpeta_familiar LIMIT 153 ";
 $result4 = mysqli_query($link,$sql4);
 $total4 = mysqli_num_rows($result4);
  if ($row4 = mysqli_fetch_array($result4)){
 mysqli_field_seek($result4,0);
 while ($field4 = mysqli_fetch_field($result4)){
-} do {
-	?>
-
+} do {?>
         L.marker([<?php echo $row4[6];?>, <?php echo $row4[7];?>]).addTo(map).bindPopup('<?php echo 'FAMILIA: '.$row4[1].'</br>'.$row4[2].'  '.$row4[3].'</br>Direccion :'.$row4[4].' Nº '.$row4[5];?>')
-
-        <?php 
+<?php 
 $numero4++;
 } while ($row4 = mysqli_fetch_array($result4));
 } else {
 }
 ?>
-
 </script>
-
 </body>
 </html>
