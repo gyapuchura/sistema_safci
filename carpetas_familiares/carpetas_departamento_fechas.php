57ba8b09ec2ba94df5cd4fe30af02e421bc5bb85
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2025-12-05 16:35:03 -0400

    reportes por fechas

---
+++ carpetas_familiares/carpetas_departamento_fechas.php
@@ -0,0 +1,331 @@
+<?php  include("../cabf.php");?>
+<?php  include("../inc.config.php");?>
+<?php 
+date_default_timezone_set('America/La_Paz');
+$fecha_ram				= date("Ymd");
+$fecha 					= date("Y-m-d");
+$gestion                = date("Y");
+
+$fecha_r = explode('-',$fecha);
+$f_emision = $fecha_r[2].'/'.$fecha_r[1].'/'.$fecha_r[0];
+
+$inicio = $_GET['inicio'];
+$finalizacion = $_GET['finalizacion'];
+
+$fecha_i = explode('-',$inicio);
+$f_inicio = $fecha_i[2].'/'.$fecha_i[1].'/'.$fecha_i[0];
+
+$fecha_f = explode('-',$finalizacion);
+$f_finalizacion = $fecha_f[2].'/'.$fecha_f[1].'/'.$fecha_f[0];
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>CARPETAS FAMILIARES DEPARTAMENTO FECHAS</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+
+    Highcharts.data({
+        csv: document.getElementById('tsv').innerHTML,
+        itemDelimiter: '\t',
+        parsed: function (columns) {
+
+            var brands = {},
+                brandsData = [],
+                versions = {},
+                drilldownSeries = [];
+
+            // Parse percentage strings
+            columns[1] = $.map(columns[1], function (value) {
+                if (value.indexOf('%') === value.length - 1) {
+                    value = parseFloat(value);
+                }
+                return value;
+            });
+
+            $.each(columns[0], function (i, name) {
+                var brand,
+                    version;
+
+                if (i > 0) {
+
+                    // Remove special edition notes
+                    name = name.split(' -')[0];
+
+                    // Split into brand and version
+                    version = name.match(/([0-9]+[\.0-9x]*)/);
+                    if (version) {
+                        version = version[0];
+                    }
+                    brand = name.replace(version, '');
+
+                    // Create the main data
+                    if (!brands[brand]) {
+                        brands[brand] = columns[1][i];
+                    } else {
+                        brands[brand] += columns[1][i];
+                    }
+
+                    // Create the version data
+                    if (version !== null) {
+                        if (!versions[brand]) {
+                            versions[brand] = [];
+                        }
+                        versions[brand].push(['v' + version, columns[1][i]]);
+                    }
+                }
+
+            });
+
+            $.each(brands, function (name, y) {
+                brandsData.push({
+                    name: name,
+                    y: y,
+                    drilldown: versions[name] ? name : null
+                });
+            });
+            $.each(versions, function (key, value) {
+                drilldownSeries.push({
+                    name: key,
+                    id: key,
+                    data: value
+                });
+            });
+
+            // Create the chart
+
+            <?php
+$sql_t =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_t = mysqli_query($link,$sql_t);
+$row_t = mysqli_fetch_array($result_t);
+$total = $row_t[0];
+
+$sql_a =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='1' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_a = mysqli_query($link,$sql_a);
+$row_a = mysqli_fetch_array($result_a);
+$beni = $row_a[0];
+
+$sql_b =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='2' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_b = mysqli_query($link,$sql_b);
+$row_b = mysqli_fetch_array($result_b);
+$cbba =    $row_b[0];
+
+$sql_c =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='3' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_c = mysqli_query($link,$sql_c);
+$row_c = mysqli_fetch_array($result_c);
+$chuquisaca =$row_c[0];
+
+$sql_d =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='4' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_d = mysqli_query($link,$sql_d);
+$row_d = mysqli_fetch_array($result_d);
+$la_paz = $row_d[0];
+
+$sql_e =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='5' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_e = mysqli_query($link,$sql_e);
+$row_e = mysqli_fetch_array($result_e);
+$oruro = $row_e[0];
+
+$sql_f =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='6' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_f = mysqli_query($link,$sql_f);
+$row_f = mysqli_fetch_array($result_f);
+$pando = $row_f[0];
+
+$sql_g =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='7' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_g = mysqli_query($link,$sql_g);
+$row_g = mysqli_fetch_array($result_g);
+$potosi =$row_g[0];
+
+$sql_h =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='8' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_h = mysqli_query($link,$sql_h);
+$row_h = mysqli_fetch_array($result_h);
+$santa_cruz = $row_h[0];
+
+$sql_i =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='9' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_i = mysqli_query($link,$sql_i);
+$row_i = mysqli_fetch_array($result_i);
+$tarija = $row_i[0];
+
+
+$beni_p         = ($beni*100)/$total;
+$cbba_p         = ($cbba*100)/$total;
+$chuquisaca_p   = ($chuquisaca*100)/$total;
+$la_paz_p       = ($la_paz*100)/$total;
+$oruro_p        = ($oruro*100)/$total;
+$pando_p        = ($pando*100)/$total;
+$potosi_p       = ($potosi*100)/$total;
+$santa_cruz_p   = ($santa_cruz*100)/$total;
+$tarija_p       = ($tarija*100)/$total;
+
+?>
+            $('#container').highcharts({
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'CANTIDAD DE CARPETAS FAMILIARES POR DEPARTAMENTO'
+                },
+                subtitle: {
+                    text: 'Fuente: Sistema MEDI-SAFCI del <?php echo $f_inicio;?> al <?php echo $f_finalizacion;?>'
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Porcetaje de un total de <?php echo $total;?>'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    series: {
+                        borderWidth: 0,
+                        dataLabels: {
+                            enabled: true,
+                            format: '{point.y:.1f}%'
+                        }
+                    }
+                },
+
+                tooltip: {
+                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> del total<br/>'
+                },
+
+                series: [{
+                    name: 'Carpetas Familiares',
+                    colorByPoint: true,
+                    data: brandsData
+                }],
+                drilldown: {
+                    series: drilldownSeries
+                }
+            });
+        }
+    });
+});
+
+
+		</script>
+	</head>
+	<body>
+<script src="../js/highcharts.js"></script>
+<script src="../js/modules/data.js"></script>
+<script src="../js/modules/drilldown.js"></script>
+
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+<!-- Data from www.netmarketshare.com. Select Browsers => Desktop share by version. Download as tsv. -->
+<pre id="tsv" style="display:none">Browser Version	Total Market Share
+
+BENI	<?php echo $beni_p;?>%
+COCHABAMBA	<?php echo $cbba_p;?>%
+CHUQUISACA 	<?php echo $chuquisaca_p;?>%
+LA PAZ	<?php echo $la_paz_p;?>%
+ORURO	<?php echo $oruro_p;?>%
+PANDO	<?php echo $pando_p;?>%
+POTOSI 	<?php echo $potosi_p;?>%
+SANTA CRUZ 	<?php echo $santa_cruz_p;?>%
+TARIJA 	<?php echo $tarija_p;?>%
+</pre>
+
+<h2 style="text-align: center; font-family: Arial; font-size: 14px; color: #2D56CF;">CANTIDAD DE CARPETAS DEL <?php echo $f_inicio;?> al <?php echo $f_finalizacion;?> </h2>
+		<table width="700" border="1" align="center" cellspacing="0">
+		  <tbody>
+		    <tr>
+		      <td width="37" style="font-family: Arial; font-size: 12px; color: #2D56CF; text-align: center;">N°</td>
+              <td width="199" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">DEPARTAMENTO</td>
+              <td width="110" style="color: #2D56CF; font-family: Arial; font-size: 12px; text-align: center;">N° CARPETAS FAMILIARES</td>
+		      <td width="101" style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">MUNICIPIOS</td>
+		     <!--- <td width="106" style="color: #2D56CF; font-size: 12px; font-family: Arial; text-align: center;">F302A</td>  --->
+	        </tr>
+            <?php
+    $numero=1; 
+    $sql =" SELECT iddepartamento, departamento FROM departamento WHERE iddepartamento !='10' ";
+    $result = mysqli_query($link,$sql);
+    if ($row = mysqli_fetch_array($result)){
+    mysqli_field_seek($result,0);           
+    while ($field = mysqli_fetch_field($result)){
+    } do {
+
+        $sql_depto =" SELECT count(iddepartamento) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND iddepartamento='$row[0]' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+        $result_depto = mysqli_query($link,$sql_depto);
+        $row_depto = mysqli_fetch_array($result_depto);
+        $carpetas_d = $row_depto[0];
+        $carpetas_depto  = number_format($carpetas_d, 0, '.', '.');
+
+
+    ?>
+		    <tr>
+		      <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $numero;?></td>
+              <td style="font-size: 12px; font-family: Arial;"><?php echo $row[1];?></td>
+              <td style="font-size: 12px; font-family: Arial; text-align: center;"><?php echo $carpetas_depto;?></td>
+		      <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">
+              <a href="carpetas_municipio_fechas.php?iddepartamento=<?php echo $row[0];?>&inicio=<?php echo $inicio;?>&finalizacion=<?php echo $finalizacion;?>" target="_blank" class="Estilo12" style="font-size: 12px; font-family: Arial;" onClick="window.open(this.href, this.target, 'width=850,height=1000,scrollbars=YES,top=60,left=400'); return false;">MUNICIPIOS</a>
+              </td>
+		     <!--- <td style="font-size: 12px; color: #2D56CF; font-family: Arial; text-align: center;">&nbsp;</td> --->
+	        </tr>
+            <?php
+        $numero=$numero+1;
+        }
+        while ($row = mysqli_fetch_array($result));
+        } else {
+        }
+        ?>
+	      </tbody>
+    </table>
+
+
+    
+<?php
+$sql_cf =" SELECT count(idcarpeta_familiar) FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_cf = mysqli_query($link,$sql_cf);
+$row_cf = mysqli_fetch_array($result_cf);  
+$total_cf  = number_format($row_cf[0], 0, '.', '.');
+?>
+
+<span style="font-family: Arial; font-size: 12px;"><h4 align="center">TOTAL DE CARPETAS FAMILIARES = <?php echo $total_cf;?> </h4></spam>
+
+<?php
+$sql_p = " SELECT idmunicipio FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' GROUP BY idmunicipio ";
+$result_p = mysqli_query($link,$sql_p);
+$municipios = mysqli_num_rows($result_p);  
+?>
+<span style="font-family: Arial; font-size: 12px;"><h4 align="center">N° DE MUNICIPIOS = <?php echo $municipios;?> </h4></spam>
+
+<?php
+$sql_mun =" SELECT idestablecimiento_salud FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' GROUP BY idestablecimiento_salud ";
+$result_mun = mysqli_query($link,$sql_mun);
+$establecimientos_p = mysqli_num_rows($result_mun);  
+$establecimientos  = number_format($establecimientos_p, 0, '.', '.');
+?>
+<span style="font-family: Arial; font-size: 12px;"><h4 align="center">N° DE ESTABLECIMIENTOS DE SALUD = <?php echo $establecimientos;?> </h4></spam>
+
+<?php
+$sql_int =" SELECT count(idintegrante_cf) FROM integrante_cf WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_int = mysqli_query($link,$sql_int);
+$row_int = mysqli_fetch_array($result_int);  
+$integrantes_p = $row_int[0];
+$integrantes  = number_format($integrantes_p, 0, '.', '.');
+?>
+<span style="font-family: Arial; font-size: 12px;"><h4 align="center">N° DE INTEGRANTES DE FAMILIA REGISTRADOS = <?php echo $integrantes;?> </h4></spam>
+
+<?php
+$sql_per = " SELECT idusuario FROM carpeta_familiar WHERE estado='CONSOLIDADO' AND fecha_registro BETWEEN '$inicio' AND '$finalizacion' GROUP BY idusuario ";
+$result_per = mysqli_query($link,$sql_per);
+$personal_p = mysqli_num_rows($result_per);  
+$personal  = number_format($personal_p, 0, '.', '.');
+
+?>
+<span style="font-family: Arial; font-size: 12px;"><h4 align="center">N° DE PERSONAL SAFCI REGISTRADOR = <?php echo $personal;?> </h4></spam>
+
+	</body>
+</html>
---
+++ sala_situacional/situacional_fechas.php
@@ -0,0 +1,248 @@
+<?php include("../cabf.php"); ?>
+<?php include("../inc.config.php"); ?>
+<?php
+date_default_timezone_set('America/La_Paz');
+$fecha_ram				= date("Ymd");
+$fecha 					= date("Y-m-d");
+$gestion 			    = date("Y");
+
+$idusuario_ss  =  $_SESSION['idusuario_ss'];
+$idnombre_ss   =  $_SESSION['idnombre_ss'];
+$perfil_ss     =  $_SESSION['perfil_ss'];
+
+$inicio = $_POST['inicio'];
+$finalizacion = $_POST['finalizacion'];
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SISTEMA MEDI-SAFCI</title>
+
+    <!-- Custom fonts for this template -->
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+
+        <?php include("../menu.php");?>
+
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include("../top_bar.php"); ?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">ANÁLISIS SITUACIONAL POR FECHAS</h1>
+                    <p class="mb-4">Esta seccion permite generar la analitica por rangos de fechas y facilitar el analisis situacional de parametros mas importantes del PROGRAMA NACIONAL SAFCI - MI SALUD.</p>
+
+            <!-- REPORTE POR FECHAS -->
+                <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">ANÁLISIS POR INTERVALO DE FECHAS</h6>
+                </div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <div class="col-sm-3">
+                        <h6 class="text-primary">DESDE:</h6>
+                        <input type="date" name="inicio" id="" class="form-control" value="<?php echo $inicio;?>" disabled>
+                        </div>
+                        <div class="col-sm-3">
+                        <h6 class="text-primary">HASTA:</h6>
+                        <input type="date" name="finalizacion" id="" class="form-control" value="<?php echo $finalizacion;?>" disabled>
+                        </div>
+                        <div class="col-sm-6">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+             <!-- GRAFICA PRODUCCION DE CARPETAS FAMILIARES -->
+                <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">PRODUCCIÓN DE CARPETAS FAMILIARES </h6>
+                </div>
+                <div class="card-body">
+
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <a class="btn btn-primary btn-icon-split" href="../carpetas_familiares/carpetas_departamento_fechas.php?inicio=<?php echo $inicio;?>&finalizacion=<?php echo $finalizacion;?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=1000,height=800,scrollbars=YES,top=50,left=300'); return false;">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-book"></i>
+                            </span>
+                            <span class="text">PRODUCCIÓN DE CARPETAS FAMILIARES </span></a>
+                        </div>
+                        <div class="col-sm-4">
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                <!-- GRAFICA SALUD DE LOS INTEGRANTES POR RANGO DE FECHAS -->
+                <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">SALUD DE LOS INTEGRANTES DE LA FAMILIA </h6>
+                </div>
+                <div class="card-body">
+
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <a class="btn btn-primary btn-icon-split" href="../carpetas_familiares/salud_integrantes_grafica_fecha.php?inicio=<?php echo $inicio;?>&finalizacion=<?php echo $finalizacion;?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=900,height=800,scrollbars=YES,top=50,left=300'); return false;">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-book"></i>
+                            </span>
+                            <span class="text">SALUD DE LOS INTEGRANTES DE LA FAMILIA</span></a>
+                        </div>
+                        <div class="col-sm-4">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- GRAFICA DETERMINANTES DE LA SALUD POR RANGO DE FECHAS -->
+                <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">RIESGO EN LAS DETERMINANTES DE LA SALUD </h6>
+                </div>
+                <div class="card-body">
+
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <a class="btn btn-primary btn-icon-split" href="../carpetas_familiares/riesgo_determinante_salud_fecha.php?inicio=<?php echo $inicio;?>&finalizacion=<?php echo $finalizacion;?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=900,height=600,scrollbars=YES,top=50,left=300'); return false;">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-book"></i>
+                            </span>
+                            <span class="text">RIÉSGO DE LAS DETERMINANTES DE LA SALUD</span></a>
+                        </div>
+                        <div class="col-sm-4">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- GRAFICA RIESGO FMILIAR POR RANGO DE FECHAS -->
+                <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">RIÉSGO FAMILIAR </h6>
+                </div>
+                <div class="card-body">
+
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <a class="btn btn-primary btn-icon-split" href="../carpetas_familiares/grafica_evaluacion_salud_familiar_fecha.php?inicio=<?php echo $inicio;?>&finalizacion=<?php echo $finalizacion;?>" target="_blank" class="Estilo12" onClick="window.open(this.href, this.target, 'width=900,height=600,scrollbars=YES,top=50,left=300'); return false;">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-book"></i>
+                            </span>
+                            <span class="text">RIÉSGO FAMILIAR</span></a>
+                        </div>
+                        <div class="col-sm-4">
+                        </div>
+                    </div>
+                </div>
+
+
+                        
+
+
+                
+
+
+           
+
+  
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Ministerio de Salud y Deportes &copy; MSYD <?php echo $gestion; ?></span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">¿ESTA SEGURO DE SALIR?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Seleccione la opcion Salir para cerrar sesion tendrá que volver a introducir su password.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="salir.php">Salir de Sistema</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+
+
+
+
+</body>
+</html>
