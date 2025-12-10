5f930767d2dd5fbe1bef134f122a3123205fd097
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2025-12-10 16:18:58 -0400

    colores y graficos

---
+++ carpetas_familiares/factores_riesgo_deptos_fecha.php
@@ -0,0 +1,165 @@
+<?php  include("../cabf.php");?>
+<?php  include("../inc.config.php");?>
+<?php 
+date_default_timezone_set('America/La_Paz');
+$fecha_ram				= date("Ymd");
+$fecha 					= date("Y-m-d");
+$gestion                = date("Y");
+
+$inicio = $_GET['inicio'];
+$finalizacion = $_GET['finalizacion'];
+
+$fecha_i = explode('-',$inicio);
+$f_inicio = $fecha_i[2].'/'.$fecha_i[1].'/'.$fecha_i[0];
+
+$fecha_f = explode('-',$finalizacion);
+$f_finalizacion = $fecha_f[2].'/'.$fecha_f[1].'/'.$fecha_f[0];
+
+$fecha_r = explode('-',$fecha);
+$f_emision = $fecha_r[2].'/'.$fecha_r[1].'/'.$fecha_r[0];
+
+$idfactor_riesgo_cf = $_GET['idfactor_riesgo_cf'];
+
+$sql_fr = " SELECT idfactor_riesgo_cf, factor_riesgo_cf FROM factor_riesgo_cf WHERE idfactor_riesgo_cf='$idfactor_riesgo_cf' ";
+$result_fr = mysqli_query($link,$sql_fr);
+$row_fr = mysqli_fetch_array($result_fr);
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>FACTORES DE RIESGO DEPARTAMENTOS</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'FACTOR DE RIESGO - <?php echo $row_fr[1];?>'
+        },
+        subtitle: {
+            text: 'Fuente: REGISTRO SISTEMA MEDI-SAFCI del <?php echo $f_inicio;?> al <?php echo $f_finalizacion;?>'
+        },
+        xAxis: {
+            categories: [
+
+                <?php 
+$numero = 0;
+$sql = " SELECT iddepartamento, departamento FROM departamento WHERE iddepartamento !='10' ORDER BY iddepartamento ";
+$result = mysqli_query($link,$sql);
+$total = mysqli_num_rows($result);
+ if ($row = mysqli_fetch_array($result)){
+mysqli_field_seek($result,0);
+while ($field = mysqli_fetch_field($result)){
+} do {
+	?>
+ '<?php  echo $row[1];?>'
+
+<?php 
+$numero++;
+if ($numero == $total) {
+echo "";
+}
+else {
+echo ",";
+}
+} while ($row = mysqli_fetch_array($result));
+} else {
+echo "";
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '<?php echo $row_fr[1];?>'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} INTEGRANTES </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+
+
+{ name: '<?php echo $row_fr[1];?>',
+
+    data: [
+<?php 
+$numero3 = 0;
+$sql3 = " SELECT iddepartamento, departamento FROM departamento WHERE iddepartamento !='10' ORDER BY iddepartamento ";
+$result3 = mysqli_query($link,$sql3);
+$total3 = mysqli_num_rows($result3);
+ if ($row3 = mysqli_fetch_array($result3)){
+mysqli_field_seek($result3,0);
+while ($field3 = mysqli_fetch_field($result3)){
+} do {
+	?>
+ 
+<?php
+$sql_a =" SELECT COUNT(integrante_factor_riesgo.idintegrante_factor_riesgo) FROM integrante_factor_riesgo, carpeta_familiar  ";
+$sql_a.=" WHERE integrante_factor_riesgo.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar  ";
+$sql_a.=" AND carpeta_familiar.iddepartamento='$row3[0]' AND integrante_factor_riesgo.idfactor_riesgo_cf='$idfactor_riesgo_cf' AND carpeta_familiar.fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_a = mysqli_query($link,$sql_a);
+$row_a = mysqli_fetch_array($result_a);
+?>
+<?php echo $row_a[0]; ?>
+<?php 
+$numero3++;
+if ($numero3 == $total3) {
+echo "";
+}
+else {
+echo ",";
+}
+} while ($row3 = mysqli_fetch_array($result3));
+} else {
+echo "";
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+
+        ]
+}
+
+    
+    ]
+    });
+});
+		</script>
+	</head>
+	<body>
+<script src="../js/highcharts.js"></script>
+<script src="../js/modules/exporting.js"></script>
+
+<div id="container" style="min-width: 410px; height: 400px; margin: 0 auto"></div>
+
+
+
+	</body>
+</html>
---
+++ carpetas_familiares/morbilidad_cf_deptos_fecha.php
@@ -0,0 +1,164 @@
+<?php  include("../cabf.php");?>
+<?php  include("../inc.config.php");?>
+<?php 
+date_default_timezone_set('America/La_Paz');
+$fecha_ram				= date("Ymd");
+$fecha 					= date("Y-m-d");
+$gestion                = date("Y");
+
+$idmorbilidad_cf = $_GET['idmorbilidad_cf'];
+
+$inicio = $_GET['inicio'];
+$finalizacion = $_GET['finalizacion'];
+
+$fecha_i = explode('-',$inicio);
+$f_inicio = $fecha_i[2].'/'.$fecha_i[1].'/'.$fecha_i[0];
+
+$fecha_f = explode('-',$finalizacion);
+$f_finalizacion = $fecha_f[2].'/'.$fecha_f[1].'/'.$fecha_f[0];
+
+$fecha_r = explode('-',$fecha);
+$f_emision = $fecha_r[2].'/'.$fecha_r[1].'/'.$fecha_r[0];
+
+$sql_mr = " SELECT idmorbilidad_cf, morbilidad_cf FROM morbilidad_cf WHERE idmorbilidad_cf='$idmorbilidad_cf' ";
+$result_mr = mysqli_query($link,$sql_mr);
+$row_mr = mysqli_fetch_array($result_mr);
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>MORBILIDAD POR DEPARTAMENTOS</title>
+
+		<script type="text/javascript" src="../sala_situacional/jquery.min.js"></script>
+		<style type="text/css">
+${demo.css}
+		</style>
+		<script type="text/javascript">
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'MORBILIDAD - <?php echo $row_mr[1];?>'
+        },
+        subtitle: {
+            text: 'Fuente: REGISTRO SISTEMA MEDI-SAFCI del <?php echo $f_inicio;?> al <?php echo $f_finalizacion;?> '
+        },
+        xAxis: {
+            categories: [
+
+                <?php 
+$numero = 0;
+$sql = " SELECT iddepartamento, departamento FROM departamento WHERE iddepartamento !='10' ORDER BY iddepartamento ";
+$result = mysqli_query($link,$sql);
+$total = mysqli_num_rows($result);
+ if ($row = mysqli_fetch_array($result)){
+mysqli_field_seek($result,0);
+while ($field = mysqli_fetch_field($result)){
+} do {
+	?>
+ '<?php  echo $row[1];?>'
+
+<?php 
+$numero++;
+if ($numero == $total) {
+echo "";
+}
+else {
+echo ",";
+}
+} while ($row = mysqli_fetch_array($result));
+} else {
+echo "";
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '<?php echo $row_mr[1];?>'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} INTEGRANTES </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [
+
+
+{ name: '<?php echo $row_mr[1];?>',
+
+    data: [
+<?php 
+$numero3 = 0;
+$sql3 = " SELECT iddepartamento, departamento FROM departamento WHERE iddepartamento !='10' ORDER BY iddepartamento ";
+$result3 = mysqli_query($link,$sql3);
+$total3 = mysqli_num_rows($result3);
+ if ($row3 = mysqli_fetch_array($result3)){
+mysqli_field_seek($result3,0);
+while ($field3 = mysqli_fetch_field($result3)){
+} do {
+	?>
+ 
+<?php
+$sql_a =" SELECT COUNT(integrante_morbilidad.idintegrante_morbilidad) FROM integrante_morbilidad, carpeta_familiar  ";
+$sql_a.=" WHERE integrante_morbilidad.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND carpeta_familiar.estado='CONSOLIDADO' AND carpeta_familiar.estado='CONSOLIDADO' ";
+$sql_a.=" AND carpeta_familiar.iddepartamento='$row3[0]' AND integrante_morbilidad.idmorbilidad_cf='$idmorbilidad_cf' AND carpeta_familiar.fecha_registro BETWEEN '$inicio' AND '$finalizacion' ";
+$result_a = mysqli_query($link,$sql_a);
+$row_a = mysqli_fetch_array($result_a);
+?>
+<?php echo $row_a[0]; ?>
+<?php 
+$numero3++;
+if ($numero3 == $total3) {
+echo "";
+}
+else {
+echo ",";
+}
+} while ($row3 = mysqli_fetch_array($result3));
+} else {
+echo "";
+/*
+Si no se encontraron resultados
+*/
+}
+?>
+
+
+        ]
+}
+
+    
+    ]
+    });
+});
+		</script>
+	</head>
+	<body>
+<script src="../js/highcharts.js"></script>
+<script src="../js/modules/exporting.js"></script>
+
+<div id="container" style="min-width: 410px; height: 400px; margin: 0 auto"></div>
+
+
+	</body>
+</html>
