05f9d94fb2360fd260b075008fed36a6f9cd8df6
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2026-02-11 16:11:42 -0400

    reporte personal prod

--- produccion_servicios/reportes_produccion_servicios.php
+++ produccion_servicios/reportes_produccion_servicios.php
@@ -121,6 +121,18 @@ $perfil_ss     =  $_SESSION['perfil_ss'];
                     </div>
                 </div> 
 
+                <div class="form-group row">
+                    <div class="col-sm-2">
+                    </div>
+                    <div class="col-sm-6">
+                        <h6 class="text-primary">5.- REPORTE POR PERFIL DE PERSONAL</h6>
+                    </div>
+                    <div class="col-sm-4">
+                    <a href="../produccion_servicios/reporte_prod_perfil_nal.php" target="_blank" onClick="window.open(this.href, this.target, 'width=1200,height=900,scrollbars=YES'); return false;">
+                    <h6 class="text-info"><i class="far fa-chart-bar"></i>MOSTRAR REPORTE</h6></a>
+                    </div>
+                </div>
+
           
                     
 <!-- END aqui va el comntenido de la pagina ---->
