10adee8d44b5a103a0b87e362eea2d6a08d47e96
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2026-04-09 22:41:00 -0400

    bandeja de beneficiarios bono juana azurduy

--- safci_perinatal/beneficiarios_bono_mun.php
+++ safci_perinatal/beneficiarios_bono_mun.php
@@ -10,6 +10,7 @@ $idmunicipio = $_POST["municipio_salud"];
                     <th>ESTABLECIMIENTO DE SALUD</th> 
                     <th>ÁREA DE INFLUENCIA</th>                                   
                     <th>CODIGO CARPETA FAMILIAR</th>
+                    <th>HISTORIA CLINICA DIGITAL</th>
                     <th>FAMILIA</th>
                     <th>NOMBRE DEL INTEGRANTE BENEFICIARIO</th>
                     <th>EDAD</th>
@@ -22,8 +23,8 @@ $idmunicipio = $_POST["municipio_salud"];
         <?php
         $numero=1; 
         $sql =" SELECT integrante_cf.idintegrante_cf, integrante_cf.idnombre, establecimiento_salud.establecimiento_salud, tipo_area_influencia.tipo_area_influencia, area_influencia.area_influencia,   ";
-        $sql.=" carpeta_familiar.codigo, carpeta_familiar.familia, nombre.nombre, nombre.paterno, nombre.materno, nombre.fecha_nac, parentesco.parentesco, carpeta_familiar.idcarpeta_familiar ";
-        $sql.=" FROM integrante_beneficiario, integrante_cf, nombre, carpeta_familiar, parentesco, establecimiento_salud,  tipo_area_influencia, area_influencia ";
+        $sql.=" carpeta_familiar.codigo, carpeta_familiar.familia, nombre.nombre, nombre.paterno, nombre.materno, nombre.fecha_nac, parentesco.parentesco, carpeta_familiar.idcarpeta_familiar,  ";
+        $sql.=" carpeta_familiar.idestablecimiento_salud, carpeta_familiar.iddepartamento, integrante_cf.idparentesco FROM integrante_beneficiario, integrante_cf, nombre, carpeta_familiar, parentesco, establecimiento_salud,  tipo_area_influencia, area_influencia ";
         $sql.=" WHERE integrante_beneficiario.idintegrante_cf=integrante_cf.idintegrante_cf AND integrante_cf.idparentesco=parentesco.idparentesco ";
         $sql.=" AND integrante_cf.idcarpeta_familiar=carpeta_familiar.idcarpeta_familiar AND integrante_cf.idnombre=nombre.idnombre ";
         $sql.=" AND carpeta_familiar.idestablecimiento_salud=establecimiento_salud.idestablecimiento_salud ";
@@ -41,7 +42,14 @@ $idmunicipio = $_POST["municipio_salud"];
                 <td><?php echo mb_strtoupper($row[3]." ".$row[4]);?></td>
                 <td>
                     <a href="../carpetas_familiares/imprime_carpeta_familiar.php?idcarpeta_familiar=<?php echo $row[12];?>" target="_blank" onClick="window.open(this.href, this.target, 'width=1400,height=800,top=50, left=200, scrollbars=YES'); return false;">
-                    <h6 class="text-info"><?php echo $row[5];?></h6></a> 
+                    <h6 class="text-primary"><?php echo $row[5];?></h6></a> 
+                </td>
+                <td>
+                    <a class="btn btn-primary btn-icon-split" href="../produccion_servicios/imprime_historia_clinica_ps.php?idcarpeta_familiar=<?php echo $row_[12];?>&idintegrante_cf=<?php echo $row[0];?>&idnombre_integrante=<?php echo $row[1];?>" target="_blank" onClick="window.open(this.href, this.target, 'width=1000,height=1000,top=50, left=400, scrollbars=YES'); return false;">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-book"></i>
+                    </span>
+                    <span class="text">HISTORIA CLÍNICA</span></a>  
                 </td>
                 <td><?php echo $row[6];?></td>
                 <td><?php echo mb_strtoupper($row[7]." ".$row[8]." ".$row[9]);?></td>
@@ -63,8 +71,16 @@ $idmunicipio = $_POST["municipio_salud"];
                 </td>
                 <td><?php echo $row[11];?></td>
                 <td>
-                <form name="FORM_RED" action="valida_benficiario_bono.php" method="post">
-                <input name="idarea_influencia" type="hidden" value="<?php echo $row[0];?>">
+                <form name="FORM_BONO" action="valida_benficiario_bono.php" method="post">
+                <input name="idintegrante_cf" type="hidden" value="<?php echo $row[0];?>">
+                <input name="idcarpeta_familiar" type="hidden" value="<?php echo $row[12];?>">
+                <input name="idestablecimiento_salud" type="hidden" value="<?php echo $row[13];?>">
+                <input name="idmunicipio" type="hidden" value="<?php echo $idmunicipio;?>">
+                <input name="iddepartamento" type="hidden" value="<?php echo $row[14];?>">
+                <input name="idnombre_integrante" type="hidden" value="<?php echo $row[1];?>">
+                <input name="edad" type="hidden" value="<?php echo $edad;?>">
+                <input name="parentesco" type="hidden" value="<?php echo $row[11];?>">
+                <input name="idparentesco" type="hidden" value="<?php echo $row[15];?>">
                 <button type="submit" class="btn btn-info btn-icon-split">
                 <span class="icon text-white-50">
                     <i class="fas fa-arrow-right"></i>
