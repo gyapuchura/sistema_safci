4e178fe3b857929bcbb7eb44fb49177d8bf1d1dc
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2026-04-17 16:36:08 -0400

    avance hcp 17 de abril

--- safci_perinatal/guarda_historia_perinatal.php
+++ safci_perinatal/guarda_historia_perinatal.php
@@ -35,7 +35,7 @@ $sql_af    = " SELECT idarea_influencia FROM carpeta_familiar WHERE idcarpeta_fa
 $result_af = mysqli_query($link,$sql_af);
 $row_af    = mysqli_fetch_array($result_af);
 
-$iddepartamento = $row_e[0];
+$iddepartamento = $row_e[0]; 
 $idred_salud    = $row_e[1];
 $idmunicipio    = $row_e[2];
 $idarea_influencia = $row_af[0];
@@ -49,42 +49,61 @@ $rowm    = mysqli_fetch_array($resultm);
 
 $correlativo = $rowm[0]+1;
 
-$codigo = "PSAFCI-ATENCION-".$correlativo."/".$gestion;
+$codigo = "MSYD-HCP-".$correlativo."/".$gestion;
 
 $anos_mayor_nivel  = $_POST['anos_mayor_nivel'];
 $vive_sola         = $_POST['vive_sola'];
 
+    foreach($_POST['idantecedente_familiar'] as $idantecedente_familiar_i) {
 
+    $sql2 = " INSERT INTO antecedente_perinatal (idhistoria_perinatal, idtipo_antecedente_enfermedad, idantecedente_enfermedad, fecha_registro, hora_registro, idusuario) ";
+    $sql2.= " VALUES ('$idhistoria_perinatal','2','$idantecedente_familiar_i','$fecha','$hora','$idusuario_ss') ";
+    $result2 = mysqli_query($link,$sql2);
 
+    }
 
+        foreach($_POST['idantecedente_personal'] as $idantecedente_personal_i) {
 
+    $sql2 = " INSERT INTO antecedente_perinatal (idhistoria_perinatal, idtipo_antecedente_enfermedad, idantecedente_enfermedad, fecha_registro, hora_registro, idusuario) ";
+    $sql2.= " VALUES ('$idhistoria_perinatal','1','$idantecedente_personal_i','$fecha','$hora','$idusuario_ss') ";
+    $result2 = mysqli_query($link,$sql2);
 
-$idtipo_atencion = $_POST['idtipo_atencion'];
-$fecha           = $_POST['fecha_registro'];
+    }
 
-$idpatologia_ap_sano = $_POST['idpatologia_ap_sano'];
-$subjetivo  = $link->real_escape_string($_POST['subjetivo']);
-$objetivo   = $link->real_escape_string($_POST['objetivo']);
-$analisis   = $link->real_escape_string($_POST['analisis']);
-$plan       = $link->real_escape_string($_POST['plan']);
+$gestaciones = $_POST['gestaciones'];
+$partos      = $_POST['partos'];
+$abortos     = $_POST['abortos'];
+$cesareas    = $_POST['cesareas'];
 
+$nacidos_vivos   = $_POST['nacidos_vivos'];
+$viven           = $_POST['viven'];
+$nacidos_muertos = $_POST['nacidos_muertos'];
+$muertos_a_semana = $_POST['muertos_a_semana'];
+$muertos_d_semana = $_POST['muertos_d_semana'];
 
+$vaginales            = $_POST['vaginales'];
+$idultimo_previo      = $_POST['idultimo_previo'];
+$antecedente_gemelos  = $_POST['antecedente_gemelos'];
+$fecha_fea            = $_POST['fecha_fea'];
 
-/******** SIGNOS VITALES  ********/
+$embarazo_planeado      = $_POST['embarazo_planeado'];
+$idmetodo_anticonceptivo  = $_POST['idmetodo_anticonceptivo'];
 
-$talla              = $_POST['talla'];
-$peso               = $_POST['peso'];
-$temperatura        = $_POST['temperatura'];
-$frec_cardiaca      = $_POST['frec_cardiaca'];
-$frec_respiratoria  = $_POST['frec_respiratoria'];
-$presion_arterial   = $_POST['presion_arterial'];
-$presion_arterial_d = $_POST['presion_arterial_d'];
-$saturacion         = $_POST['saturacion'];
-$alergia            = $_POST['alergia'];
-$descripcion_alergia       = $link->real_escape_string($_POST['descripcion_alergia']);
+$peso_anterior   = $_POST['peso_anterior'];
+$idesno          = $_POST['idesno'];
+$fuma_activo     = $_POST['fuma_activo'];
+$fuma_pasivo     = $_POST['fuma_pasivo'];
+$drogas          = $_POST['drogas'];
+$alcohol         = $_POST['alcohol'];
+$violencia       = $_POST['violencia'];
 
+$idantirubeola   = $_POST['idantirubeola'];
+$antitetanica    = $_POST['antitetanica'];
+$dosis_antitetanica = $_POST['dosis_antitetanica'];
+$ex_odontologico    = $_POST['ex_odontologico'];
+$ex_mamas           = $_POST['ex_mamas'];
 
-
+/******** GUARDA HISTORIA PERINATAL  ********/
 
 
 $sql0 = " INSERT INTO atencion_psafci (iddepartamento, idred_salud, idmunicipio, idestablecimiento_salud, idnombre, edad, idgenero, ";
@@ -93,6 +112,8 @@ $sql0.= " VALUES ('$iddepartamento','$idred_salud','$idmunicipio','$idestablecim
 $sql0.= " '$idrepeticion','$idtipo_consulta','$idtipo_atencion','$idnacion','$codigo','$correlativo','$gestion', '$fecha','$hora','$idusuario_ss')";
 $result0 = mysqli_query($link,$sql0);   
 $idatencion_psafci = mysqli_insert_id($link);
+
+
 
 $sql_dg = " INSERT INTO diagnostico_psafci (idatencion_psafci, subjetivo, objetivo, analisis, plan, motivo_consulta, idpatologia, fecha_registro, hora_registro, idusuario) ";
 $sql_dg.= " VALUES ('$idatencion_psafci','$subjetivo','$objetivo','$analisis','$plan','$motivo_consulta','$idpatologia_ap_sano','$fecha','$hora','$idusuario_ss') ";
