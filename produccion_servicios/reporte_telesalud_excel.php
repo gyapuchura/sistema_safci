dff990e35824c567ee17f3e69b85e35010744d28
Luis Gonzalo Yapuchura Chuquimia <gonzalo.yapuchura1982@gmail.com>
2026-06-16 17:12:05 -0400

    avance 16 junio

--- produccion_servicios/reporte_telesalud_excel.php
+++ produccion_servicios/reporte_telesalud_excel.php
@@ -1,3 +1,8 @@
+<?php	
+header('Content-type: application/vnd.ms-excel');
+header("Content-Disposition: attachment; filename=REPORTE ATENCIONES TELESALUD.xls");
+header("Pragma: no-cache");
+header("Expires: 0");?>
 <?php include("../cabf.php");?>
 <?php include("../inc.config.php");?>
 <?php
